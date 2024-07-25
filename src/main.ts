5709bbabc525ff4a58dea0dab033dade10f014da
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2024-07-24 23:46:53 -0300

    install swagger y config of main

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,15 @@ async function bootstrap() {
       transform: true, //transformar los datos siempre que pueda
     }),
   );
+
+  const config = new DocumentBuilder()
+  .setTitle('Cloudinary NestJS')
+  .setDescription('The Cloudinary API description')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('docs', app, document);
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT_SERVER') || 3001;
