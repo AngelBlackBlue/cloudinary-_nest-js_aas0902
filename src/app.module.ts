0423f6243f89413fae507461634b60ba1e2c8d83
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2024-07-24 23:29:22 -0300

    modulo data base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { FileModule } from './file/file.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule} from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 
 @Module({
@@ -9,20 +9,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USER'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
-      inject: [ConfigService],
-    }),
+    DatabaseModule,
     FileModule,
     // CloudinaryModule,
   ],
